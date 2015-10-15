948ad05ad91edffad7c83115c9bac96036863315
scipper <scipper@myscipper.de>
2015-10-15 09:47:52 +0200

    error reporting

--- src/Formparser.php
+++ src/Formparser.php
@@ -15,6 +15,13 @@ class Formparser {
 	
 	/**
 	 * 
+	 * @var integer
+	 */
+	const FORMPARSER_UPLOAD_ERR_FORBIDDEN = 101;
+	
+	
+	/**
+	 * 
 	 * @var array
 	 */
 	protected $files;
@@ -45,7 +52,13 @@ class Formparser {
 		foreach($this->files as $inputName => $file) {
 			if(is_array($file["name"])) {
 				foreach($file["name"] as $k => $v) {
+					if(substr($file["name"][$k], 0, 1) == ".") {
+						$this->reportError(self::FORMPARSER_UPLOAD_ERR_FORBIDDEN);
+						continue;
+					}
+					
 					if($file["error"][$k] != 0) {
+						$this->reportError($file["error"][$k]);
 						continue;
 					}
 					
@@ -61,7 +74,13 @@ class Formparser {
 				continue;
 			}
 			
+			if(substr($file["name"], 0, 1) == ".") {
+				$this->reportError(self::FORMPARSER_UPLOAD_ERR_FORBIDDEN);
+				continue;
+			}
+			
 			if($file["error"] != 0) {
+				$this->reportError($file["error"]);
 				continue;
 			}
 			
@@ -73,6 +92,43 @@ class Formparser {
 			$formfile->setSize($file["size"]);
 			
 			$this->formfiles[] = $formfile;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param integer $code
+	 */
+	public function reportError($code) {
+		switch ($code) {
+			case UPLOAD_ERR_INI_SIZE: {
+				error_log("the uploaded file exceeds the upload_max_filesize directive in php.ini");
+			} break;
+			case UPLOAD_ERR_FORM_SIZE: {
+				error_log("the uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form");
+			} break;
+			case UPLOAD_ERR_PARTIAL: {
+				error_log("the uploaded file was only partially uploaded");
+			} break;
+			case UPLOAD_ERR_NO_FILE: {
+				error_log("no file was uploaded");
+			} break;
+			case UPLOAD_ERR_NO_TMP_DIR: {
+				error_log("missing a temporary folder");
+			} break;
+			case UPLOAD_ERR_CANT_WRITE: {
+				error_log("failed to write file to disk");
+			} break;
+			case UPLOAD_ERR_EXTENSION: {
+				error_log("file upload stopped by extension");
+			} break;
+			case self::FORMPARSER_UPLOAD_ERR_FORBIDDEN: {
+				error_log("uploading this type of file is forbidden");
+			} break;
+		
+			default: {
+				error_log("Unknown upload error");
+			} break;
 		}
 	}
 	
